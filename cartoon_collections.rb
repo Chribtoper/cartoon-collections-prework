109b5dbd47422bf1f70e109193cd722f9daf698c
Chribtoper <xcstolfax@gmail.com>
2018-08-08 20:55:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ end
 end
 
 def summon_captain_planet(array)
-  newarr = array.collect {|x| x.upcase + "!" }
+  newarr = array.collect |x| 
+  x.upcase + "!"
 end
 
 def long_planeteer_calls# code an argument here
